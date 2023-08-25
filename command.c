1f906f5c194a2b54b1f7bd3638536dc7389cd64f
Brian <chumobrian@gmail.com>
2023-08-25 14:37:22 +0300

    fix: executables in current dir

--- command.c
+++ command.c
@@ -1,5 +1,33 @@
 #include "main.h"
 /**
+ * com_sort - sorts command names
+ * @name: name of command
+ * Return: path to the respective command program
+ */
+char *com_sort(char *name)
+{
+	char *temp = NULL, cwd[1024], *cmd_name, *cmd = NULL;
+
+	temp = getcwd(cwd, sizeof(cwd));
+	temp = malloc(sizeof(char) * (_strlen(cwd) + 1));
+	if (temp == NULL)
+		return (NULL);
+	_strcpy(temp, cwd);
+	cmd_name = strtok(name, ".~/");
+
+	/*generate full path to command executable*/
+	cmd = malloc(sizeof(char) * (_strlen(name) + _strlen(temp) + 2));
+	if (cmd != NULL)
+	{
+		_strcpy(cmd, temp);
+		_strcat(cmd, "/");
+		_strcat(cmd, cmd_name);
+		_strcat(cmd, "\0");
+	}
+	free(temp);
+	return (cmd);
+}
+/**
  * com_path - returns pathname to command
  * @cmd_name: name of command
  * Return: pathname to command
@@ -8,6 +36,10 @@ char *com_path(char *cmd_name)
 {
 	char *cmd, *path = "/bin/";
 
+	if (_strncmp(cmd_name, "./", 2) == 0)
+	{
+		return (com_sort(cmd_name));
+	}
 
 	cmd = malloc(sizeof(char) * (_strlen(cmd_name) + _strlen(path) + 1));
 	if (cmd == NULL)
